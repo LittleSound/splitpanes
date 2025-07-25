07b30889422d47635eeec555c0d9f78d6f31ef68
Rizumu Ayaka <rizumu@ayaka.moe>
2025-07-25 23:35:04 +0800

    fix emit event types

--- src/components/splitpanes/index.ts
+++ src/components/splitpanes/index.ts
@@ -2,4 +2,18 @@ import Splitpanes from './splitpanes.vue'
 import Pane from './pane.vue'
 
 export { Splitpanes, Pane }
-export type { PaneData, TouchState, SplitterTaps, DragPosition, PaneSums, PaneResizeVariables, SplitpanesProps, PaneProps } from '@/types'
+export type { 
+  PaneData, 
+  TouchState, 
+  SplitterTaps, 
+  DragPosition, 
+  PaneSums, 
+  PaneResizeVariables, 
+  SplitpanesProps, 
+  PaneProps,
+  SplitpanesEmits,
+  BaseEventPayload,
+  EventWithIndex,
+  EventWithPane,
+  EventWithPanes
+} from '@/types'
